4380a4d9eba9003941d09cb681629345259effe2
Juan Mayorga <js.mayorga368@gmail.com>
2021-10-26 15:39:58 -0500

    create country repo and updated data processing. Coral reefs done.

---
+++ .Rprofile
@@ -0,0 +1,1 @@
+source("scripts/00_setup.R")
---
+++ scripts/00_setup.R
@@ -0,0 +1,12 @@
+##########################
+## Paths to directories ##
+##########################
+
+# Check for OS
+sys_path <- easyR::get_google_drive_path()
+
+# Path to emLab data folder
+emlab_data_path <- file.path(sys_path,"Shared drives/emlab/data")
+
+# Path to this project folder
+project_path <- file.path(sys_path,"Shared drives/emlab/projects/current-projects/ps-colombia")
