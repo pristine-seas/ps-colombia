3c30a29082a5801f4effab1cff5122900dae1913
Juan Mayorga <js.mayorga368@gmail.com>
2022-01-06 12:38:35 -0500

    major update, including GFW SAR analysis for Seaflower

--- scripts/00_setup.R
+++ scripts/00_setup.R
@@ -3,10 +3,60 @@
 ##########################
 
 # Check for OS
-sys_path <- easyR::get_google_drive_path()
+sys_path <- easyR::get_google_drive_path("juanmayorga@ucsb.edu")
 
 # Path to emLab data folder
 emlab_data_path <- file.path(sys_path,"Shared drives/emlab/data")
 
 # Path to this project folder
 project_path <- file.path(sys_path,"Shared drives/emlab/projects/current-projects/ps-colombia")
+
+# Mapping theme
+
+theme_map <- function(...) {
+  theme_minimal() +
+    theme(
+      text = element_text(family = default_font_family,
+                          color = default_font_color),
+      # remove all axes
+      # add a subtle grid
+      panel.grid.major = element_line(color = "#dbdbd9", size = 0.2),
+      panel.grid.minor = element_blank(),
+      # background colors
+      plot.background = element_rect(fill = default_background_color,
+                                     color = NA),
+      panel.background = element_rect(fill = default_background_color,
+                                      color = NA),
+      legend.background = element_rect(fill = default_background_color,
+                                       color = NA),
+      # borders and margins
+      plot.margin = unit(c(.5, .5, .2, .5), "cm"),
+      panel.border = element_blank(),
+      panel.spacing = unit(c(-.1, 0.2, .2, 0.2), "cm"),
+      # titles
+      legend.title = element_text(size = 11),
+      legend.text = element_text(size = 9, hjust = 0,
+                                 color = default_font_color),
+      plot.title = element_text(size = 15, hjust = 0.5,
+                                color = default_font_color),
+      plot.subtitle = element_text(size = 10, hjust = 0.5,
+                                   color = default_font_color,
+                                   margin = margin(b = -0.1,
+                                                   t = -0.1,
+                                                   l = 2,
+                                                   unit = "cm"),
+                                   debug = F),
+      # captions
+      plot.caption = element_text(size = 7,
+                                  hjust = .5,
+                                  margin = margin(t = 0.2,
+                                                  b = 0,
+                                                  unit = "cm"),
+                                  color = "#939184"),
+      ...
+    )
+}
+
+default_font_family <- "Arial"
+default_font_color <- '#22211d'
+default_background_color <- "#f5f5f2"
